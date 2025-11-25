8d7fbbd5d31e0a9e63d98388e067e067a990b96b
YUTSUKI <dddfckmmdmdm@gmail.com>
2025-11-25 13:42:54 +0800

    GUI 自動提交

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 using namespace std;
 
+ long long factorial(int x){
+    if(x>1){
+        return x*factorial(x-1);
+    }
+    else{
+        return 1;
+    }
+ }
+int sum(int x){
+    if(x>0){
+        return x+sum(x-1);
+    }
+    else{
+        return 0;
+    }
+}
+
 int main() {
-    
-    
+    long long x,y;
+    cin>>x;
+    cout << factorial(x) << endl << sum(x);  
     return 0;
 }
