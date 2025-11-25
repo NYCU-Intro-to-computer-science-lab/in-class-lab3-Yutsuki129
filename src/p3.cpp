268fc384c61e9081444c987e21d659c91fe391b6
YUTSUKI <dddfckmmdmdm@gmail.com>
2025-11-25 14:13:20 +0800

    GUI 自動提交

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,9 +3,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
-    
-    return 0;
+bool isPrime(int n){
+    if(n==0||n==1){
+        return false;
+    }
+    else{
+     for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;}
+     }
+     return true;
+} 
 }
+int main() {
+
+    int n,a,b;
+    cin >> n;
+    for(int i=1;i<=(n/2);i++){
+            a=isPrime(i);
+            b=isPrime(n-i);
+            if(a==true&&b==true){
+            cout << i << " " << n-i;
+            break;
+        } 
+        }
+        return 0;
+    } 
